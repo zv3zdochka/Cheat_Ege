a20c594010111f699c143dc62d41ff87be53eeb1
PUMOVETZ <batsiev2006@gmail.com>
2024-03-25 22:20:38 +0300

    logging

--- Search.py
+++ Search.py
@@ -12,11 +12,12 @@ try:
     from aiogram.enums import ParseMode
     from aiogram.filters import CommandStart
     import json
+    import logging
 
 except ModuleNotFoundError:
     sys.exit("Required libraries are missing. Please install them using:\n"
              "pip install -r requirements.txt\n"
-             "You can find the requirements.txt file at: https://github.com/zv3zdochka/LAW.git")
+             "You can find the requirements.txt file at: https://github.com/zv3zdochka/Cheat_Ege.git")
 
 TOKEN = "7104080784:AAFiU0STuHAsW-KsFOF5cwVozmdn1UCflB0"
 auth_waiting = []
@@ -26,6 +27,8 @@ ban = []
 nicknames = {}
 dp = Dispatcher()
 bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+
+logger = logging.getLogger("log.txt")
 
 proxies = {
     "http": "http://45.11.20.11:3000",
@@ -57,19 +60,19 @@ class PageChecker:
                     text = BeautifulSoup(html, 'html.parser').get_text()
 
                     if len(text) == 118:
-                        return f"Page not found: {page_id}"
+                        logging.warning(f"Page {url} not found in time {datetime.datetime.now()}")
+                        return
 
                     for target in self.targets:
                         if target in text:
-                            return f"Target {target} found on page {page_id}"
+                            return [page_id, target]
 
                     if "JavaScript" in text:
-                        return f"JavaScript detected on the page {id}"
+                        logging.warning(f"Java on page {url} in time {datetime.datetime.now()}")
+                        return
 
         except Exception as e:
-            with open('log.txt', 'a') as f:
-                f.write(f"Exception processing page {page_id}. On time {datetime.datetime.now()}\n")
-            return f"Error processing page {page_id}: {e}"
+            logging.warning(f"Exeption {e} on {url} in time {datetime.datetime.now()}")
 
     async def fetch_all(self, ids):
         async with aiohttp.ClientSession() as session:
@@ -83,8 +86,7 @@ class PageChecker:
             results = await self.fetch_all(chunk)
             for page_id, result in zip(chunk, results):
                 if result:
-                    self.founded.append([self.subject_name, result])
-                    print(result)
+                    self.founded.append([result[0], result[1]])
             if None in chunk:
                 break
 
@@ -100,14 +102,28 @@ class PageChecker:
             while True:
                 yield None
 
+    def generate_test(self):
+        problems = {1: 1}
+        dif = {f'prob{i}': problems[i] for i in problems}
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f'{self.subject_url}/test?a=generate', params=dif,
+                                   allow_redirects=False) as response:
+                location_header = response.headers.get('location')
+                if location_header:
+                    page_id = location_header.split('id=')[1].split('&nt')[0]
+                    self.current_num = int(page_id)
+                else:
+                    logging.error(f"Failed to generate test {datetime.datetime.now()}")
+                    exit()
+
     def get_current_test_num(self):
         try:
             sdamgia = SdamGIA()
             self.current_num = int(sdamgia.generate_test(self.subject_name, {1: 1}))
             del sdamgia
         except KeyError:
-            with open('log.txt', 'a') as f:
-                f.write(f"Vpn or proxy error. On time {datetime.datetime.now()}\n")
+            logging.error(f"VPN error {datetime.datetime.now()}")
             exit("Switch off your VPN and try again")
 
     @staticmethod
@@ -141,19 +157,21 @@ async def command_start_handler(message: Message) -> None:
 
     elif message.chat.id not in admins and message.chat.id not in users:
         await message.answer(f"Hello, {hbold(message.from_user.full_name)} !\nAuthorization in progress.")
+        logging.info(f"User {message.chat.username} requests authorisation {datetime.datetime.now()}")
         auth_waiting.append(message.chat.id)
         await bot.send_message(1363003331,
                                f"New user requesting authorization \nId: {message.chat.id} \nName: {message.chat.username}")
 
     elif message.chat.id not in admins and message.chat.id in users:
-        await message.answer(f"Hello, {hbold(message.from_user.full_name)}! You are authorized, wait for test.")
-
+        await message.answer(f"Hello, {hbold(message.from_user.full_name)}! You are authorized, wait for updates.")
+        logging.info(f"User {message.chat.username} authorised {datetime.datetime.now()}")
     elif message.chat.id in admins:
         await message.answer(f"Hello, {hbold(message.from_user.full_name)} !\n"
-                             f"You are an admin.\n"
-                             f"Waiting for authorization:\n")
-        for i in auth_waiting:
-            await message.answer(i)
+                             f"You are an admin.\n")
+        if auth_waiting:
+            await message.answer(f"Waiting for authorization:\n")
+            for i in auth_waiting:
+                await message.answer(i)
 
 
 @dp.message()
@@ -165,7 +183,7 @@ async def echo_handler(message: types.Message) -> None:
 
     elif message.chat.id not in admins:
         await message.answer("Only admins can chat with the bot.")
-
+        logging.info(f"User {message.chat.username} messages {message.text} on time {datetime.datetime.now()}")
     else:
         text = message.text.split()
         if text[0] == 'allow':
@@ -185,12 +203,13 @@ async def echo_handler(message: types.Message) -> None:
                 auth_waiting.remove(int(text[1]))
                 await bot.send_message(int(text[1]), str("Permission denied."))
                 await bot.send_message(1363003331, f"Authorisation of user {text[1]} denied")
+                logging.info(f"User`s {nicknames.get(str(text[1]))} request denied {datetime.datetime.now()}")
                 update_json()
             else:
                 await message.answer("No such user, waiting for authorization.")
 
         elif text[0] == 'text':
-            await tell_user(text[1])
+            await send_users(text[1])
 
         elif text[0] == 'users':
             await bot.send_message(1363003331, str(users))
@@ -215,19 +234,22 @@ async def echo_handler(message: types.Message) -> None:
             except ValueError:
                 pass
 
-            await bot.send_message(1363003331, f"User {text[1]} was successfully baned. ")
+            await bot.send_message(1363003331, f"User {text[1]} was successfully banned. ")
             update_json()
+            logging.info(f"User`s {nicknames.get(str(text[1]))} banned {datetime.datetime.now()}")
 
         elif text[0] == 'free':
             ban.remove(int(text[1]))
             await bot.send_message(1363003331, f"User {text[1]} is free. ")
             update_json()
+            logging.info(f"User`s {nicknames.get(str(text[1]))} is free {datetime.datetime.now()}")
 
         elif text[0] == 'delete':
             await bot.send_message(int(text[1]), 'Administrator deleted you.')
             users.remove(int(text[1]))
             await bot.send_message(1363003331, f"User {text[1]} was successfully deleted. ")
             update_json()
+            logging.info(f"User`s {nicknames.get(str(text[1]))} deleted {datetime.datetime.now()}")
 
         else:
             await bot.send_message(1363003331, f"No such command")
@@ -247,9 +269,10 @@ def update_json():
         exit(e)
 
 
-async def tell_user(text: str):
+async def send_users(text: str):
     for i in users:
         await bot.send_message(i, text)
+    logging.info(f"Message {text} sent to users {datetime.datetime.now()}")
 
 
 async def main() -> None:
@@ -257,14 +280,14 @@ async def main() -> None:
 
 
 async def broadcaster(st: str):
+    logging.info(f"Broadcast {st} {datetime.datetime.now()}")
     try:
-        await tell_user(st)
+        await send_users(st)
     except Exception as e:
         exit(e)
 
 
 async def search():
-    # main loop
     sleep_time = 15
     while True:
         s_t = time.time()
@@ -273,41 +296,40 @@ async def search():
             pch = PageChecker(key, value[0], value[1])
             await pch.search_from_to()
             for j in pch.founded:
-                print(j)
-                await broadcaster(str(j))
+                logging.info(f"Test {j} found in subject {pch.subject_name} {datetime.datetime.now()}")
+                await broadcaster(f"Subject: {pch.subject_name}\n"
+                                  f"Id: {j[0]}\n"
+                                  f"Target: {j[1]}")
+
                 with open('found.txt', 'a') as f:
                     f.write(f"Found in subject {j[0]}: {j[1]}. On time {datetime.datetime.now()}\n")
+
             n_d = data[pch.subject_name]
             n_d[1] = pch.current_num
             data[pch.subject_name] = n_d
             del pch
-            print('sub')
-        with open(inp[0], 'w') as file:
+
+        with open('data.json', 'w') as file:
             json.dump(data, file, ensure_ascii=False)
-        print('cycle')
-        if time.time() - s_t < 5:
-            sleep_time += 1
-        elif time.time() - s_t > 10:
+
+        if time.time() - s_t > 10:
             sleep_time -= 1
-        print(sleep_time)
+
         await asyncio.sleep(sleep_time)
 
 
 if __name__ == "__main__":
-    inp = sys.argv[1:]
-    if len(inp) != 1:
-        with open('log.txt', 'a') as f:
-            f.write(f"Wrong command. On time {datetime.datetime.now()}\n")
-        exit('Usage: python Search.py nameoffile.json quantity_of_threads')
+    if len(sys.argv[1:]) != 0:
+        logging.critical(f"wrong command {datetime.datetime.now()}")
+        exit('Usage: python Search.py')
     try:
-        with open(inp[0], 'r', errors='ignore', encoding="windows-1251") as file:
+        with open('data.json', 'r', errors='ignore', encoding="windows-1251") as file:
             data = json.load(file)
             for key, value in data.items():
                 data[key] = [value[0], value[1]]
 
     except FileNotFoundError:
-        with open('log.txt' 'a') as f:
-            f.write(f"No data file. On time {datetime.datetime.now()}\n")
+        logging.critical(f"No data.json in time {datetime.datetime.now()}")
         exit("Add data.json file")
     try:
         with open('users.json', 'r', errors='ignore') as file:
@@ -319,6 +341,7 @@ if __name__ == "__main__":
             ban = data_bot.get('ban')
 
     except FileNotFoundError:
+        logging.critical(f"No users.json in time {datetime.datetime.now()}")
         exit("Add users.json file")
 
 

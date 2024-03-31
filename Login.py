38fce5ca2d50364d1af4f9ac9ab3e3032b468c94
PUMOVETZ <batsiev2006@gmail.com>
2024-03-31 12:06:13 +0300

    Pdf grab

--- Login.py
+++ Login.py
@@ -1,51 +1,71 @@
 import time
-import yaml
-from yaml.loader import SafeLoader
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+import json
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
-class ReshuEgeSolver:
-    settings = dict()
+chrome_options = webdriver.ChromeOptions()
 
-    def __init__(self):
-        with open("settings.yml", 'r') as stream:
-            self.settings = yaml.load(stream, SafeLoader)
+settings = {
+    "recentDestinations": [{
+        "id": "Save as PDF",
+        "origin": "local",
+        "account": ""
+    }],
+    "selectedDestinationId": "Save as PDF",
+    "version": 2,
+    "isHeaderFooterEnabled": False,
+    "isCssBackgroundEnabled": True
+}
 
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")  # Включаем Headless режим
-        self.driver = webdriver.Chrome(options=chrome_options)  # Используем Chrome
+chrome_options.add_argument('--enable-print-browser')
+#chrome_options.add_argument('--headless')
 
-        self.exam_url = 'https://math-ege.sdamgia.ru/test?id=74921366'
+prefs = {
+    'printing.print_preview_sticky_settings.appState': json.dumps(settings),
+    'savefile.default_directory': r'C:\Users\batsi\PycharmProjects\Ege_Cheater'
+}
+chrome_options.add_argument('--kiosk-printing')
+chrome_options.add_experimental_option('prefs', prefs)
 
-    def __log_in(self):
+
+class PdfLeaker:
+    def __init__(self, url):
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.url = url
+        self.login = "he@gmail.com"
+        self.password = "123"
+
+    def log_in(self):
         self.driver.get("https://ege.sdamgia.ru")
-
+        time.sleep(5)
         email_field = self.driver.find_element(By.ID, "email")
-        email_field.send_keys(self.settings['PROFILE']['EMAIL'])
+        email_field.send_keys(self.login)
         password_field = self.driver.find_element(By.ID, "current-password")
-        password_field.send_keys(self.settings['PROFILE']['PASSWORD'])
+        password_field.send_keys(self.password)
         password_field.submit()
         time.sleep(0.4)
 
-    def __solve_test(self):
-        self.driver.get(self.exam_url)
-
-        # Ждем некоторое время, чтобы страница полностью загрузилась
-        time.sleep(5)
-
-        # Получаем HTML-код страницы
-        html = self.driver.page_source
-
-        # Сохраняем HTML в файл
-        with open("page.html", "w", encoding="utf-8") as f:
-            f.write(html)
+    def leak(self):
+        # self.driver.get(self.url)
+        # time.sleep(0.4)
+        #
+        # link = self.driver.find_element(By.LINK_TEXT, 'Версия для печати и копирования в MS Word')
+        #
+        # link.click()
+        # time.sleep(1)
+        self.driver.get(f"{self.url}&print=true")
+        time.sleep(2)
+        self.driver.find_element(By.CSS_SELECTOR, "input#cb_ans").click()
+        self.driver.find_element(By.CSS_SELECTOR, "input#cb_sol").click()
+        self.driver.execute_script('window.print();')
+        self.driver.close()
 
     def run(self):
-        self.__log_in()
-        self.__solve_test()
+        self.log_in()
+        self.leak()
+        print('Requests completed')
+
 
 if __name__ == "__main__":
-    solver = ReshuEgeSolver()
+    solver = PdfLeaker('https://math-ege.sdamgia.ru/test?id=74921366')
     solver.run()
